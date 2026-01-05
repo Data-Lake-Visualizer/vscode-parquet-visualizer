a682a8b986f31b47e5624e87af7ce026310346c3
lucien <lucienmartijn@gmail.com>
2026-01-05 18:31:29 +0700

    chore: update build scripts to handle DuckDB bindings as externals

--- build.bat
+++ build.bat
@@ -5,14 +5,14 @@ rmdir /s /q "out"
 mkdir out
 mkdir out\binding
 
-@REM REM Copy required files
-copy /y node_modules\duckdb\lib\binding\duckdb.node out\binding\
-if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+@REM REM Copy required files - DuckDB bindings are now handled as externals
+@REM copy /y node_modules\duckdb\lib\binding\duckdb.node out\binding\
+@REM if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 @REM Run esbuild for extension.ts
 echo Run esbuild for extension.ts
-esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
+esbuild src\extension.ts --bundle --outfile=out\extension.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --external:@duckdb/node-api --external:@duckdb/node-bindings --external:@duckdb/node-bindings-* --format=cjs --platform=node --minify --define:process.env.AZURE_APP_INSIGHTS_CONNECTION_STRING="\"%AZURE_APP_INSIGHTS_CONNECTION_STRING%\""
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
--- build_worker.bat
+++ build_worker.bat
@@ -1,9 +1,9 @@
 @echo on
 
-@REM REM Copy required files
-copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node out\binding\
+@REM REM Copy required files - DuckDB bindings are now handled as externals
+@REM copy /y node_modules\duckdb-async\node_modules\duckdb\lib\binding\duckdb.node out\binding\
 copy /y node_modules\parquet-wasm\node\parquet_wasm_bg.wasm out\
 
 @REM Run esbuild for worker.ts
 echo Run esbuild for worker.ts
-esbuild src\worker.ts --bundle --outfile=out\worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --format=cjs --platform=node --minify
+esbuild src\worker.ts --bundle --outfile=out\worker.js --external:vscode --external:nock --external:aws-sdk --external:mock-aws-s3 --external:@duckdb/node-api --external:@duckdb/node-bindings --external:@duckdb/node-bindings-* --format=cjs --platform=node --minify
